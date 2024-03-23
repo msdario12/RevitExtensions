eb16e33c9538ca7fc5f2dca7d2e1a6933d0dd7ca
msdario12 <mansilladario94@gmail.com>
2024-03-23 15:35:03 -0300

    Finish basic functionality, create section view of each type.

--- CreateSectionsView.cs
+++ CreateSectionsView.cs
@@ -17,24 +17,24 @@ namespace RevitExtensions
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            // Get the handle of current document.
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            Transaction transaction = new Transaction(doc, "Create new section view");
             try
             {
                 // Run application
-                // Get the handle of current document.
-                Document doc = commandData.Application.ActiveUIDocument.Document;
-                Transaction transaction = new Transaction(doc, "Modify Columns");
-
-                transaction.Start();
-
                 TaskDialog.Show("Inicio de Crear Vistas", "Solo una prueba");
-                // 💻 Get all elements (columns) instances of each type.
 
+                // 💻 Get all elements (columns) instances of each type.
                 IList<Element> columns = new FilteredElementCollector(doc).
                     OfCategory(BuiltInCategory.OST_StructuralColumns).
                     WhereElementIsNotElementType().
                     ToElements();
+
                 // TODO: redefine types in dict.
                 Dictionary<string, Element> dict_columns = new Dictionary<string, Element>();
+
+                transaction.Start();
 
                 foreach (var column in columns)
                 {
@@ -58,18 +58,60 @@ namespace RevitExtensions
                     // Calculate vector based in host
                     // In case of column use FacingOrientation
                     XYZ faceOrientation = (columnTuple.Value as FamilyInstance).FacingOrientation;
-                    XYZ vector = faceOrientation - column_origin;
+                    XYZ vector = faceOrientation;
                     // Get element size
                     ElementId elementId = columnTuple.Value.GetTypeId();
                     ElementType type = doc.GetElement(elementId) as ElementType;
-                    // Width based in parameter
-                    double elementWidth = type.GetParameters("b").First().AsDouble();
 
                     BoundingBoxXYZ boundingBox = columnTuple.Value.get_BoundingBox(null);
+                    // Width based in parameter
+                    //double elementWidth = type.GetParameters("b").First().AsDouble();
+                    double elementWidth = boundingBox.Max.X - boundingBox.Min.X;
                     // Height based in bounding box
                     double elementHeight = boundingBox.Max.Z - boundingBox.Min.Z;
                     // Video en 9:22
+                    double elementDepth = UnitUtils.ConvertToInternalUnits(40, UnitTypeId.Centimeters);
+                    double offset = UnitUtils.ConvertToInternalUnits(40, UnitTypeId.Centimeters);
+
+                    //4 Create Transform (Origin Point + X,Y,Z vectors)
+                    Transform transformIdentity = Transform.Identity;
+                    transformIdentity.Origin = column_origin;
+
+                    // Normalize vectors
+                    vector = vector.Normalize();
+
+                    transformIdentity.BasisX = vector;
+                    transformIdentity.BasisY = XYZ.BasisZ;
+                    transformIdentity.BasisZ = vector.CrossProduct(XYZ.BasisZ);
+
+                    // Create new boundary box
+                    BoundingBoxXYZ sectionBox = new BoundingBoxXYZ();
+                    double half = elementWidth / 2;
+
+                    sectionBox.Min = new XYZ(-half - offset, 0 - offset, -elementDepth);
+                    sectionBox.Max = new XYZ(+half + offset, elementHeight + offset, elementDepth);
+                    // Apply transforms to section box
+                    sectionBox.Transform = transformIdentity;
+
+                    //6 Create sections view.
+                    ElementId sectionTypeId = doc.GetDefaultElementTypeId(ElementTypeGroup.ViewTypeSection);
+                    ViewSection sectionView = ViewSection.CreateSection(doc, sectionTypeId, sectionBox);
+
+                    string viewName = $"API_{columnTuple.Key}";
+                    for (int i = 0; i < 10; i++)
+                    {
+                        try
+                        {
+                            sectionView.Name = viewName;
+                            break;
+                        }
+                        catch (Exception)
+                        {
+                            viewName += "*";
+                        }
+                    }
                 }
+                transaction.Commit();
                 return Autodesk.Revit.UI.Result.Succeeded;
 
             }
@@ -77,6 +119,7 @@ namespace RevitExtensions
             {
                 message = e.Message;
                 TaskDialog.Show("Error en Execute", $"{e.Message} \n {e.StackTrace}");
+                transaction.RollBack();
                 return Autodesk.Revit.UI.Result.Failed;
             }
         }
